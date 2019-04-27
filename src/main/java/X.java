26a474ac372d27128d45344567acd7611551fd18
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-04-27 13:44:59 +0200

    more draft examples

--- src/main/java/X.java
+++ src/main/java/X.java
@@ -12,13 +12,13 @@ public class X {
                 Case($(2), "two"),
                 Case($(3), "three"),
                 Case($(), "?"));
-    }    
-    
+    }
+
     static String ads2(int input) {
         Range<Integer> threshold1 = Range.closed(0, 50);
         Range<Integer> threshold2 = Range.closed(51, 150);
         Range<Integer> threshold3 = Range.atLeast(151);
-                
+
         return Match(input).of(
                 Case($(threshold1::contains), "one"),
                 Case($(threshold2::contains), "two"),
@@ -26,8 +26,32 @@ public class X {
                 Case($(), "?"));
     }
 
+    static void ads3(Person person) {
+        switch (person.type) {
+            case VIP:
+                System.out.println("VIP"); // getfullstatistics
+                break;
+            case ORDINARY:
+                System.out.println("ORDINARY");
+                break;
+            case TEMPORARY:
+                System.out.println("TEMPORARY"); // getfaststatistics
+                break;
+            default:
+                throw new IllegalStateException("value not supported");
+        }
+    }
+
     public static void main(String[] args) {
         int input = 2;
         System.out.println(X.ads(input));
     }
+}
+
+class Person {
+    Type type;
+}
+
+enum Type {
+    ORDINARY, VIP, TEMPORARY
 }
