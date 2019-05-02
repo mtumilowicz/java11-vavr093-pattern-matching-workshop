1dc44b522338871dbcf0cfbec34654b11aaa1075
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-02 23:00:42 +0200

    todo list update

--- src/main/java/workshops/Answers.java
+++ src/main/java/workshops/Answers.java
@@ -32,6 +32,7 @@ TO-DO:
 1. add personByType pattern
 1. additional tests for eitherDecompose + PersonService
 1. optionDecompose, tryDecompose - better examples (maybe sth from workshops, try - recover, option - ?)
+1. optionDecompose -> push to the display
 1. Workshop (with switch / case, if)
 1. readme
  */
