875a53a02188cc0edbd1e23878b21b73358e0c5a
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-03 14:21:16 +0200

    copy Answers -> Workshop, copy AnswersTests -> WorkshopTests

---
+++ src/main/java/workshops/Workshop.java
@@ -0,0 +1,204 @@
+package workshops;
+
+import com.google.common.collect.Range;
+import credit.CreditAssessmentService;
+import io.vavr.CheckedRunnable;
+import io.vavr.collection.Seq;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+import lombok.NonNull;
+import out.Display;
+import person.*;
+import person.request.PersonRequest;
+import person.request.ValidPersonRequest;
+import tax.TaxService;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import static io.vavr.API.*;
+import static io.vavr.API.$;
+import static io.vavr.Patterns.*;
+import static io.vavr.Patterns.$Failure;
+import static io.vavr.Predicates.*;
+import static io.vavr.Predicates.anyOf;
+import static java.util.function.Predicate.not;
+import static workshops.DecompositionAnswersPatterns.$LocalDate;
+import static workshops.DecompositionAnswersPatterns.$PersonByCreditAssessSubjects;
+
+/**
+ * Created by mtumilowicz on 2019-05-03.
+ */
+public class Workshop {
+    public static int numberConverter(String number) {
+        return Match(number).of(
+                Case($("one"), 1),
+                Case($("two"), 2),
+                Case($("three"), 3),
+                Case($(), () -> {
+                    throw new IllegalStateException("value not supported");
+                }));
+    }
+
+    public static String thresholds(int input) {
+        Range<Integer> threshold1 = Range.closed(0, 50);
+        Range<Integer> threshold2 = Range.closed(51, 150);
+        Range<Integer> threshold3 = Range.atLeast(151);
+
+        return Match(input).of(
+                Case($(i -> i < 0), () -> {
+                    throw new IllegalArgumentException("only positive numbers!");
+                }),
+                Case($(threshold1::contains), "threshold1"),
+                Case($(threshold2::contains), "threshold2"),
+                Case($(threshold3::contains), "threshold3")
+        );
+    }
+
+    public static String switchOnEnum(@NonNull Person person) {
+        return Match(person.getType()).of(
+                Case($(PersonType.VIP), () -> StatsService.getFullStats(person)),
+                Case($(PersonType.REGULAR), () -> StatsService.getStats(person)),
+                Case($(PersonType.TEMPORARY), () -> StatsService.getFastStats(person)),
+                Case($(), ignore -> {
+                    throw new IllegalStateException("value not supported");
+                }));
+    }
+
+    public static LocalDate rawDateMapper(String date) {
+        return Match(date).of(
+                Case($(isNull()), () -> null),
+                Case($(isNotNull()), LocalDate::parse));
+    }
+
+    public static Option<LocalDate> optionDateMapper(String date) {
+        return Match(date).option(
+                Case($(isNotNull()), LocalDate::parse));
+    }
+
+    public static Either<String, Person> eitherDecompose(Either<PersonRequest, ValidPersonRequest> either) {
+        return Match(either).of(
+                Case($(isNull()), () -> Either.left("cannot be null")),
+                Case($Left($()), PersonService::patch),
+                Case($Right($()), PersonService::assemblePerson)
+        );
+    }
+
+    public static void optionDecompose(int id, Display display) {
+        Match(PersonRepository.findById(id)).of(
+                Case($None(), run(() -> display.push("cannot find person with id = " + id))),
+                Case($Some($()), value -> run(() -> display.push("person: " + value + " processed")))
+        );
+    }
+
+    public static void tryDecompose(String number, Display display) {
+        Try<Integer> _try = Try.of(() -> Integer.parseInt(number));
+        Match(_try).of(
+                Case($Success($()),
+                        i -> run(() -> display.push("squared number is: " + i * i))),
+                Case($Failure($()),
+                        ex -> run(() -> display.push("cannot square number: " + ex.getLocalizedMessage())))
+        );
+    }
+
+    public static String ifSyntax(Person person) {
+        return Match(person).of(
+                Case($(isNull()), "cannot be null"),
+                Case($(Person::isActive), PersonService::disable),
+                Case($(), PersonService::activate)
+        );
+    }
+
+    public static int getTaxRateFor(@NonNull LocalDate date) {
+        return Match(date).of(
+                Case($LocalDate($(year -> year < 2015), $(), $()), TaxService::taxBefore2015),
+                Case($LocalDate($(year -> year > 2015), $(), $()), TaxService::taxAfter2015)
+        );
+    }
+
+    public static Integer personDecompose(@NonNull Person person) {
+        return Match(person).of(
+                Case($PersonByCreditAssessSubjects($(), $()),
+                        (account, address) ->
+                                CreditAssessmentService.serviceMethodAssess(CreditAssessSubjects.builder()
+                                        .balance(account.getBalance())
+                                        .salary(account.getSalary())
+                                        .country(address.getCountry())
+                                        .build()
+                                ))
+        );
+    }
+
+    public static Either<Seq<Throwable>, Seq<Integer>> existsTest(@NonNull Seq<Try<Integer>> list) {
+        return Match(list).of(
+                Case($(exists(Try::isFailure)),
+                        tries -> Either.left(tries
+                                .filter(Try::isFailure)
+                                .map(Try::getCause)
+                                .toList())),
+                Case($(),
+                        tries -> Either.right(tries
+                                .filter(Try::isSuccess)
+                                .map(Try::get)
+                                .toList()))
+        );
+    }
+
+    public static Either<Seq<Throwable>, Seq<Integer>> forAllTest(@NonNull Seq<Try<Integer>> list) {
+        return Match(list).of(
+                Case($(forAll(Try::isSuccess)),
+                        tries -> Either.right(tries
+                                .filter(Try::isSuccess)
+                                .map(Try::get)
+                                .toList())),
+                Case($(),
+                        tries -> Either.left(tries
+                                .filter(Try::isFailure)
+                                .map(Try::getCause)
+                                .toList()))
+        );
+    }
+
+    public static String allOfTest(@NonNull Person person) {
+        return Match(person).of(
+                Case($(allOf(Person.hasType(PersonType.VIP), Person::isActive)), "vip + active"),
+                Case($(allOf(Person.hasType(PersonType.VIP), not(Person::isActive))), "vip + not active"),
+                Case($(allOf(Person.hasType(PersonType.REGULAR), Person::isActive)), "regular + active"),
+                Case($(allOf(Person.hasType(PersonType.REGULAR), not(Person::isActive))), "regular + not active"),
+                Case($(allOf(Person.hasType(PersonType.TEMPORARY), Person::isActive)), "temporary + active"),
+                Case($(allOf(Person.hasType(PersonType.TEMPORARY), not(Person::isActive))), "temporary + not active"),
+                Case($(), () -> {
+                    throw new IllegalArgumentException("case not supported");
+                })
+        );
+    }
+
+    public static String instanceOfTest(@NonNull CheckedRunnable runnable) {
+        try {
+            runnable.run();
+            return "no exception";
+        } catch (Throwable exx) {
+            return Match(exx).of(
+                    Case($(instanceOf(IllegalArgumentException.class)), "IllegalArgumentException"),
+                    Case($(instanceOf(RuntimeException.class)), "RuntimeException"),
+                    Case($(instanceOf(IOException.class)), "IOException"),
+                    Case($(), "handle rest")
+            );
+        }
+    }
+
+    public static String noneOfTest(@NonNull Person person) {
+        return Match(person).of(
+                Case($(noneOf(Person.hasType(PersonType.VIP), Person::hasBigSalary)), "handle rest"),
+                Case($(), "handle special")
+        );
+    }
+
+    public static String anyOfTest(@NonNull Person person) {
+        return Match(person).of(
+                Case($(anyOf(Person.hasType(PersonType.VIP), Person::hasBigSalary)), "handle special"),
+                Case($(), "handle rest")
+        );
+    }
+}
---
+++ src/test/groovy/WorkshopTests.groovy
@@ -0,0 +1,423 @@
+import io.vavr.collection.List
+import io.vavr.control.Either
+import io.vavr.control.Option
+import io.vavr.control.Try
+import out.Display
+import person.*
+import person.request.PersonRequest
+import person.request.ValidPersonRequest
+import spock.lang.Specification
+import workshops.Workshop
+
+import java.time.LocalDate
+import java.time.format.DateTimeParseException
+
+/**
+ * Created by mtumilowicz on 2019-04-10.
+ */
+class WorkshopTests extends Specification {
+    def "numberConverter"() {
+        expect:
+        Workshop.numberConverter('one') == 1
+        Workshop.numberConverter('two') == 2
+        Workshop.numberConverter('three') == 3
+    }
+
+    def "numberConverter guard"() {
+        when:
+        Workshop.numberConverter(null)
+
+        then:
+        thrown(IllegalStateException)
+
+        when:
+        Workshop.numberConverter('four')
+
+        then:
+        thrown(IllegalStateException)
+    }
+
+    def "thresholds"() {
+        expect:
+        Workshop.thresholds(0) == 'threshold1'
+        Workshop.thresholds(25) == 'threshold1'
+        Workshop.thresholds(50) == 'threshold1'
+        and:
+        Workshop.thresholds(51) == 'threshold2'
+        Workshop.thresholds(100) == 'threshold2'
+        Workshop.thresholds(150) == 'threshold2'
+        and:
+        Workshop.thresholds(200) == 'threshold3'
+        Workshop.thresholds(250) == 'threshold3'
+        Workshop.thresholds(1_000_000) == 'threshold3'
+    }
+
+    def "thresholds guard"() {
+        when:
+        Workshop.thresholds(-1)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "switchOnEnum"() {
+        expect:
+        Workshop.switchOnEnum(Person.builder().type(PersonType.VIP).build()) == 'full stats'
+        Workshop.switchOnEnum(Person.builder().type(PersonType.REGULAR).build()) == 'just stats'
+        Workshop.switchOnEnum(Person.builder().type(PersonType.TEMPORARY).build()) == 'fast stats'
+    }
+
+    def "switchOnEnum guard"() {
+        when:
+        Workshop.switchOnEnum(Person.builder().build())
+
+        then:
+        thrown(IllegalStateException)
+    }
+
+    def "rawDateMapper"() {
+        expect:
+        Workshop.rawDateMapper('2014-10-10') == LocalDate.of(2014, 10, 10)
+        !Workshop.rawDateMapper(null)
+    }
+
+    def "rawDateMapper exceptional"() {
+        when:
+        Workshop.rawDateMapper('wrong')
+
+        then:
+        thrown(DateTimeParseException)
+    }
+
+    def "optionDateMapper"() {
+        expect:
+        Workshop.optionDateMapper('2014-10-10') == Option.some(LocalDate.of(2014, 10, 10))
+        Workshop.optionDateMapper(null) == Option.none()
+    }
+
+    def "optionDateMapper exceptional"() {
+        when:
+        Workshop.optionDateMapper('wrong')
+
+        then:
+        thrown(DateTimeParseException)
+    }
+
+    def "eitherDecompose - successful patch when salary < 0 - active = false, salary = 0"() {
+        given:
+        def canBeFixed = PersonRequest.builder()
+                .type(PersonType.VIP)
+                .balance(3000)
+                .country('Poland')
+                .city('Warsaw')
+                .active(true)
+                .salary(-1000)
+                .build()
+        when:
+        def fixed = Workshop.eitherDecompose(Either.left(canBeFixed))
+
+        then:
+        fixed == Either.right(Person.builder()
+                .type(PersonType.VIP)
+                .active(false)
+                .account(Account.builder()
+                        .salary(Salary.of(0))
+                        .balance(3000)
+                        .build())
+                .address(Address.builder()
+                        .city('Warsaw')
+                        .country('Poland')
+                        .build())
+                .build())
+    }
+
+    def "eitherDecompose - argument cannot be null"() {
+        expect:
+        Workshop.eitherDecompose(null) == Either.left('cannot be null')
+    }
+
+    def "eitherDecompose - failure patch - to many errors (empty request)"() {
+        given:
+        def cannotBeFixed = PersonRequest.builder().build()
+
+        when:
+        def notFixed = Workshop.eitherDecompose(Either.left(cannotBeFixed))
+
+        then:
+        notFixed == Either.left('cannot be fixed, too many errors')
+    }
+
+    def "eitherDecompose - successful assemble - all business rules are met"() {
+        given:
+        def allBusinessRulesAreMet = ValidPersonRequest.builder()
+                .type(PersonType.VIP)
+                .active(true)
+                .salary(Salary.of(0))
+                .balance(3000)
+                .city('Warsaw')
+                .country('Poland')
+                .build()
+
+        when:
+        def correct = Workshop.eitherDecompose(Either.right(allBusinessRulesAreMet))
+
+        then:
+        correct == Either.right(Person.builder()
+                .type(PersonType.VIP)
+                .active(true)
+                .account(Account.builder()
+                        .salary(Salary.of(0))
+                        .balance(3000)
+                        .build())
+                .address(Address.builder()
+                        .city('Warsaw')
+                        .country('Poland')
+                        .build())
+                .build())
+    }
+
+    def "eitherDecompose - failure assemble - not all business rules are met: VIP should be active"() {
+        given:
+        def notAllBusinessRulesAreMet = ValidPersonRequest.builder()
+                .type(PersonType.VIP)
+                .active(false)
+                .salary(Salary.of(0))
+                .balance(3000)
+                .city('Warsaw')
+                .country('Poland')
+                .build()
+
+        when:
+        def incorrect = Workshop.eitherDecompose(Either.right(notAllBusinessRulesAreMet))
+
+        then:
+        incorrect == Either.left('not all business rules are matched')
+    }
+
+    def "optionDecompose, cannot find in database"() {
+        given:
+        def display = new Display()
+        def notExistsId = 2
+
+        when:
+        Workshop.optionDecompose(notExistsId, display)
+
+        then:
+        display.message == "cannot find person with id = ${notExistsId}"
+    }
+
+    def "optionDecompose, found in database"() {
+        given:
+        def display = new Display()
+        def existsId = 1
+
+        when:
+        Workshop.optionDecompose(existsId, display)
+
+        then:
+        display.message == "person: ${existsId} processed"
+    }
+
+    def "tryDecompose, successfully parsed and squared"() {
+        given:
+        def display = new Display()
+
+        when:
+        Workshop.tryDecompose('3', display)
+        
+        then:
+        display.message == 'squared number is: 9'
+    }
+    
+    def "tryDecompose, fail to parse an input string as a number"() {
+        given:
+        def display = new Display()
+
+        when:
+        Workshop.tryDecompose('wrong', display)
+        
+        then:
+        display.message == 'cannot square number: For input string: "wrong"'
+    }
+
+    def "ifSyntax"() {
+        given:
+        def activePerson = Person.builder().active(true).build()
+        def inactivePerson = Person.builder().active(false).build()
+
+        expect:
+        Workshop.ifSyntax(null) == 'cannot be null'
+        Workshop.ifSyntax(inactivePerson) == 'activated'
+        Workshop.ifSyntax(activePerson) == 'deactivated'
+    }
+
+    def "getTaxRateFor"() {
+        given:
+        def _2010_10_10 = LocalDate.of(2010, 10, 10)
+        def _2019_10_10 = LocalDate.of(2019, 10, 10)
+
+        expect:
+        Workshop.getTaxRateFor(_2010_10_10) == 15
+        Workshop.getTaxRateFor(_2019_10_10) == 25
+    }
+
+    def "decomposePerson"() {
+        given:
+        def _800 = Salary.of(800)
+        def _2000 = Salary.of(2000)
+        def _950 = Salary.of(950)
+        and:
+        def p1 = Person.builder()
+                .account(Account.builder().salary(_800).balance(20_000).build())
+                .address(Address.builder().country('POLAND').build())
+                .build()
+        def p2 = Person.builder()
+                .account(Account.builder().salary(_2000).balance(1000).build())
+                .address(Address.builder().country('USA').build())
+                .build()
+        def p3 = Person.builder().account(Account.builder().salary(_950).balance(15_000).build())
+                .address(Address.builder().country('POLAND').build())
+                .build()
+
+        expect:
+        Workshop.personDecompose(p1) == 508
+        Workshop.personDecompose(p2) == 328
+        Workshop.personDecompose(p3) == 508
+    }
+
+    def "existsTest"() {
+        given:
+        def ex1 = new IllegalArgumentException('a')
+        def ex2 = new IllegalStateException('b')
+        and:
+        def withFailures = List.of(Try.success(1),
+                Try.success(2),
+                Try.success(3),
+                Try.failure(ex1),
+                Try.failure(ex2))
+        def withoutFailures = List.of(Try.success(1),
+                Try.success(2),
+                Try.success(3),
+                Try.success(4),
+                Try.success(5))
+
+        when:
+        def test1 = Workshop.existsTest(withFailures)
+        def test2 = Workshop.existsTest(withoutFailures)
+
+        then:
+        test1.isLeft()
+        test1.getLeft().size() == 2
+        test1.getLeft().containsAll([ex1, ex2])
+        test2.isRight()
+        test2.get().size() == 5
+        test2.get().containsAll([1, 2, 3, 4, 5])
+    }
+
+    def "forAllTest"() {
+        given:
+        def ex1 = new IllegalArgumentException('a')
+        def ex2 = new IllegalStateException('b')
+        and:
+        def withFailures = List.of(Try.success(1),
+                Try.success(2),
+                Try.success(3),
+                Try.failure(ex1),
+                Try.failure(ex2))
+        def withoutFailures = List.of(Try.success(1),
+                Try.success(2),
+                Try.success(3),
+                Try.success(4),
+                Try.success(5))
+
+        when:
+        def test1 = Workshop.forAllTest(withFailures)
+        def test2 = Workshop.forAllTest(withoutFailures)
+
+        then:
+        test1.isLeft()
+        test1.getLeft().size() == 2
+        test1.getLeft().containsAll([ex1, ex2])
+        test2.isRight()
+        test2.get().size() == 5
+        test2.get().containsAll([1, 2, 3, 4, 5])
+    }
+
+    def "allOfTest"() {
+        given:
+        def vipActive = Person.builder().type(PersonType.VIP).active(true).build()
+        def vipNotActive = Person.builder().type(PersonType.VIP).active(false).build()
+        def regularActive = Person.builder().type(PersonType.REGULAR).active(true).build()
+        def regularNotActive = Person.builder().type(PersonType.REGULAR).active(false).build()
+        def temporaryActive = Person.builder().type(PersonType.TEMPORARY).active(true).build()
+        def temporaryNotActive = Person.builder().type(PersonType.TEMPORARY).active(false).build()
+
+        expect:
+        Workshop.allOfTest(vipActive) == 'vip + active'
+        Workshop.allOfTest(vipNotActive) == 'vip + not active'
+        Workshop.allOfTest(regularActive) == 'regular + active'
+        Workshop.allOfTest(regularNotActive) == 'regular + not active'
+        Workshop.allOfTest(temporaryActive) == 'temporary + active'
+        Workshop.allOfTest(temporaryNotActive) == 'temporary + not active'
+    }
+
+    def "instanceOfTest"() {
+        given:
+        def noExceptionThrower = {}
+        def illegalArgumentExceptionThrower = { throw new IllegalArgumentException() }
+        def runtimeExceptionThrower = { throw new RuntimeException() }
+        def iOExceptionThrower = { throw new IOException() }
+        def classNotFoundExceptionThrower = { throw new ClassNotFoundException() }
+
+        expect:
+        Workshop.instanceOfTest(noExceptionThrower) == 'no exception'
+        Workshop.instanceOfTest(illegalArgumentExceptionThrower) == 'IllegalArgumentException'
+        Workshop.instanceOfTest(runtimeExceptionThrower) == 'RuntimeException'
+        Workshop.instanceOfTest(iOExceptionThrower) == 'IOException'
+        Workshop.instanceOfTest(classNotFoundExceptionThrower) == 'handle rest'
+    }
+
+    def "noneOfTest"() {
+        given:
+        def _1 = Salary.of(1)
+        def _3000 = Salary.of(3000)
+        def _300 = Salary.of(300)
+        and:
+        def vip = Person.builder().type(PersonType.VIP)
+                .account(Account.builder().salary(_1).build())
+                .build()
+        def bigSalary = Person.builder().type(PersonType.REGULAR)
+                .account(Account.builder().salary(_3000).build())
+                .build()
+        def regular = Person.builder().type(PersonType.REGULAR)
+                .account(Account.builder().salary(_300).build())
+                .build()
+
+        expect:
+        Workshop.noneOfTest(vip) == 'handle special'
+        Workshop.noneOfTest(bigSalary) == 'handle special'
+        Workshop.noneOfTest(regular) == 'handle rest'
+    }
+
+    def "anyOfTest"() {
+        given:
+        def _1 = Salary.of(1)
+        def _3000 = Salary.of(3000)
+        def _300 = Salary.of(300)
+        and:
+        def vip = Person.builder().type(PersonType.VIP)
+                .account(Account.builder().salary(_1).build())
+                .build()
+        def bigSalary = Person.builder().type(PersonType.REGULAR)
+                .account(Account.builder().salary(_3000).build())
+                .build()
+        def regular = Person.builder().type(PersonType.REGULAR)
+                .account(Account.builder().salary(_300).build())
+                .build()
+
+        expect:
+        Workshop.anyOfTest(vip) == 'handle special'
+        Workshop.anyOfTest(bigSalary) == 'handle special'
+        Workshop.anyOfTest(regular) == 'handle rest'
+    }
+}
